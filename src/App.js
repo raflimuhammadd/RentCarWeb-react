33f510ce474125a2953371a49c560af6d70c319d
raflimuhammadd <raflimuhh09@gmail.com>
2022-08-13 14:26:51 +0700

    ADD: FIRST

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavBar from './components/navbar/NavBar';
+import Hero from './components/hero/Hero';
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Hero />
 
     </div>
   );
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from './Hero.module.css'
+import {AiOutlineSearch} from 'react-icons/ai'
+
+
+const Hero = () => {
+  return (
+    <div className={styles.Hero}>
+        <form>
+            <div className={styles.text}>
+                <label>Where</label>
+                <input className={styles.text_input} type="text" placeholder="Search Location"></input>
+            </div>
+            <div className={styles.from}>
+                <span className={styles.border}></span>
+                <label>From</label>
+                <input type="date" />
+            </div>
+            <div className={styles.until}>
+                <span className={styles.border}></span>
+                <label>Until</label>
+                <input type="date">
+                </input>
+            </div>
+            <div className={styles.search_btn}>
+                <button className={styles.btn}>Search for cars</button>
+                <AiOutlineSearch size={25} classname={styles.icon}/>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Hero
